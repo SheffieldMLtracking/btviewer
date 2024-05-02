fd420f875229b565218e9852c6bae70037913f96
Shi Min Chua <shiminc@gmail.com>
2024-05-02 10:13:04 +0100

    initial structure more folders

--- btviewer/__init__.py
+++ btviewer/__init__.py
@@ -1,4 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from importlib.metadata import version
 
-import btviewer.views
+__version__ = version("btviewer")
+
+from flask import Flask
+app = Flask(__name__) # The Flask application instance is created as a global variable
+
+from btviewer import views
+# declare the import after initiate the app instance, to avoid circular import,https://flask.palletsprojects.com/en/2.3.x/patterns/packages/
