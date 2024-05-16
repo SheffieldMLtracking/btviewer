ae3a8763eec351ce5af94c5db17b3619fd242311
shiminc <shiminc@gmail.com>
2024-05-16 15:48:14 +0100

    adding module to components

--- frontend/src/components/Nextbutton.jsx
+++ frontend/src/components/Nextbutton.jsx
@@ -1,13 +1,14 @@
-export default function NextButton({list,current_status}) {
-
+export default function NextButton(props) {
+    console.log(props.current_status)
     function handleClick() {
-            current_status = current_status + 1
+            current_status = props.current_status + 3
     }
 
     return (
         <div>
             <button onClick={handleClick}>
-                {list[current_status]}
+                props.list[current_status]
+                current_status
                 
             </button>
         </div>
