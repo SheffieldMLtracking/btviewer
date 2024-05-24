b5b73f0ae5e4b1244fa3d2df60ed6d65146d73ff
Joe Heffer <j.heffer@sheffield.ac.uk>
2024-05-24 13:38:11 +0100

    fix: CLI actions

--- backend/btviewer/__main__.py
+++ backend/btviewer/__main__.py
@@ -7,22 +7,34 @@ from pathlib import Path
 import btviewer.app_factory
 
 DESCRIPTION = """
-TODO
+This is a browser-based app for viewing and human labelling of tracking images.
 """
 
 USAGE = """
-TODO
+btviewer ~/my_data/
 """
 
 
 def get_args() -> argparse.Namespace:
+    """
+    Configure command-line arguments.
+    """
+
     parser = argparse.ArgumentParser(description=DESCRIPTION, usage=USAGE)
+
+    # Actions
+    # https://docs.python.org/dev/library/argparse.html#action
     parser.add_argument('-v', '--verbose', action='store_true')
-    parser.add_argument('--version', action='store_true')
+    parser.add_argument('--version', action='version', version=f'%(prog)s {btviewer.__version__}')
+
+    # Flask options
     parser.add_argument('--debug', type=bool, default=True)
     parser.add_argument('--host', default='127.0.0.1')
     parser.add_argument('--port', default=5000)
+
+    # btviewer options
     parser.add_argument('root_directory', type=Path)
+
     return parser.parse_args()
 
 
