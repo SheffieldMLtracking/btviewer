5c571a3eeb22eca47825e2d7c8212121710c23f8
shiminc <shiminc@gmail.com>
2024-06-17 11:44:55 +0100

    fetched jpeg

--- frontend/src/components/PhotoSelection.jsx
+++ frontend/src/components/PhotoSelection.jsx
@@ -4,10 +4,32 @@ import {useState} from 'react'
 Display a drop-down list of photos to be shown.
 */
 function PhotoSelection (props) {
-
-
+    const [currentPhoto, setCurrentPhoto] = useState('')
+    /*
+    List the photos for users to choose from
+    */
     let listDisplayed = props.data.map(item =>
         <option key={item.id} value={item}>{item}</option>);
+
+    /*
+    When the user selects a photo, retrieve it to send to image component
+    */
+    function photoFetcher(e) {
+        let selectedPhoto = e.target.value;
+        setCurrentPhoto(selectedPhoto);
+        console.log(selectedPhoto);
+
+        // Update the list of photo filenames available in that session
+        let url = `/api/${selectedPhoto}`;
+        url = url.replace("np","jpeg")
+        console.log(url)
+        fetch(url)
+        .then(response => response.json())
+        .then(data => setCurrentPhoto(data));
+
+
+    }
+
 
 
     return(
@@ -15,9 +37,13 @@ function PhotoSelection (props) {
             <select
                 name="photo"
                 id="photo"
+                onChange={photoFetcher}
+
             >
                 <option/>
                 {listDisplayed}</select>
+                
+
         </>
     )
 }
