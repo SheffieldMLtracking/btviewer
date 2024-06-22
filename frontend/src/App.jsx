abce0773e0815db7d184bea915675b22c6f2cd11
shiminc <shiminc@gmail.com>
2024-06-22 18:39:53 +0100

    shortcut key and create layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
-import Image from './components/Image.jsx';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 import Sessions from './components/Sessions.jsx';
 
 function App() {
     return (
         <div className="App">
-            <Sessions/>  {/*It should be a component, which in turn have all the children component to fetch session, camera, device etc.*/}
+            <Container>    
+            <Row>
+                <Col sm={4}>
+                    <h1>Btviewer</h1>
+                    <h4>Instructions</h4>
+                    <p>Zoom In: mouseclick + alt</p>
+                    <p>Zoom Out: mouseclick + windows/cmd</p>
+                    <p>Confident tag: mouseclick + ctrl</p>
+                    <p>Unconfident tag: mouseclick + shift</p>
+                </Col>
+                <Col sm={8}>
+                <Sessions/>  {/*It should be a component, which in turn have all the children component to fetch session, camera, device etc.*/}
+
+                </Col>
+            </Row>
+                  
+                    
+            </Container>    
+
         </div>
     );
 }
