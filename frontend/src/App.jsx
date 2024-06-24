09c49c47116c1f534262f4760bfe22b59cea840d
shiminc <shiminc@gmail.com>
2024-06-24 18:44:00 +0100

    instructions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
 import Sessions from './components/Sessions.jsx';
+import Instructions from './components/Instructions.jsx';
 
 function App() {
     return (
@@ -13,16 +11,10 @@ function App() {
             <Container>    
             <Row>
                 <Col sm={4}>
-                    <h1>Btviewer</h1>
-                    <h4>Instructions</h4>
-                    <p>Zoom In: mouseclick + alt</p>
-                    <p>Zoom Out: mouseclick + windows/cmd</p>
-                    <p>Confident tag: mouseclick + ctrl</p>
-                    <p>Unconfident tag: mouseclick + shift</p>
+                    <Instructions/>
                 </Col>
                 <Col sm={8}>
-                <Sessions/>  {/*It should be a component, which in turn have all the children component to fetch session, camera, device etc.*/}
-
+                    <Sessions/>  {/*It should be a component, which in turn have all the children component to fetch session, camera, device etc.*/}
                 </Col>
             </Row>
                   
---
+++ frontend/src/components/Instructions.jsx
@@ -0,0 +1,14 @@
+function Instructions () {
+    return(
+        <>
+        <h1>Btviewer</h1>
+        <h4>Instructions</h4>
+        <p>Zoom In: mouseclick + alt</p>
+        <p>Zoom Out: mouseclick + windows/cmd</p>
+        <p>Confident tag: mouseclick + ctrl</p>
+        <p>Unconfident tag: mouseclick + shift</p>
+        <p>You can drag the image after zooming in by moving the mouse pointer while keeping left-click</p>
+        </>
+    )
+}
+export default Instructions
