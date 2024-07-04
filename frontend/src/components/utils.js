f1aa28e108cddccb812fe6a80da448337ac5aa1f
shiminc <shiminc@gmail.com>
2024-07-04 10:57:06 +0100

    not working

--- frontend/src/components/utils.js
+++ frontend/src/components/utils.js
@@ -3,7 +3,11 @@ export function SaveMarkers(point, photoPath) { //JS for  utilities functoin
     const source = "btviewer";
     const version = "0.0.0";
     const url = `/api/labels/create?path=${photo_path}&source=${source}&version=${version}`;
-    console.log('in savemarkers function', point)
+    //http://localhost:5000/labels/create?path=1970-01-01/set_A/device_1234/camera_1/20200101_094359.123456_000002.np
+
+
+    console.log('in savemarkers function '+ point)
+    console.log('url sent ' + url)
     fetch(url, {
       method: "post",
       headers: { "Content-type": "application/json" },
