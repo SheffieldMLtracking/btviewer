6c1f490d1b74dcc0fc99eb20c3fb5ab1066618ab
shiminc <shiminc@gmail.com>
2024-07-10 13:27:46 +0100

    comment out development mode

--- backend/btviewer/app.py
+++ backend/btviewer/app.py
@@ -8,6 +8,6 @@ from pathlib import Path
 from .app_factory import create_app
 
 # Use test data
-ROOT_DIRECTORY = os.getenv('ROOT_DIRECTORY', Path(__file__).parent.parent.joinpath('tests/data'))
+#ROOT_DIRECTORY = os.getenv('ROOT_DIRECTORY', Path(__file__).parent.parent.joinpath('tests/data'))
 
-app = create_app(root_directory=ROOT_DIRECTORY)
+#app = create_app(root_directory=ROOT_DIRECTORY)
